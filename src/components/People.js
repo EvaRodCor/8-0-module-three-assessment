6ba6b66d082cd9909ab2debb8aa171331b537b52
Eva Rodriguez <evarodriguez@Evas-MBP.lan>
2021-12-17 18:21:38 -0500

    made some minor adjusments

--- src/components/People.js
+++ src/components/People.js
@@ -26,52 +26,46 @@ const URL = "https://ghibliapi.herokuapp.com/people"
 
 handleChange = (e)=> {
     e.preventDefault()
-        this.setState({
-            person: e.target.value
-        })
-    }
+    this.setState({
+        person: e.target.value
+    })
+}
+
 
 
 
 handleSubmit = (e) => {
     e.preventDefault()
-    fetch (URL)
-    .then((response) => response.json())
-    .then((data) => {
-    this.setState({
-        data: data
-        })
+    const people = this.state.data.find((people) => {
+        return people.name.includes(this.state.person)
     })
+
+this.setState({ person: "", people: people, isFound: true })
 }
 
 
+
+
 render () {
-
-    const personInfo = this.state.data && this.state.data.map((person) => {
-            if (person.name.toLowerCase() === this.state.person.toLowerCase()) {
-                return  (
-                <div key={person.id}>
-                <h2>Name: {person.name}</h2>
-                <p>Age: {person.age }</p>
-                <p>Gender: {person.gender}</p>
-                </div>
-                )
-            }
-            return "Not Found"
-        })
-
-return (
-
-        <div className='people'>
-            <h2>Search for a Person</h2>
-            <form onSubmit={this.handleSubmit}>
-            <input  onChange={this.handleChange}  id="values" name="search-person" type="text" />
+return(   
+    <div className='people'>
+        <h2>Search for a Person</h2>
+        <form onSubmit={this.handleSubmit}>
+            <input  onChange={this.handleChange}  value={this.state.person} type="text" />
             <button type="submit">Submit</button>
-            </form>
-            {personInfo}
+        </form>
+            {this.state.people && (
+                <div key={this.state.people.id}>
+                <h2>Name: {this.state.people.name}</h2>
+                <p>Age: {this.state.people.age }</p>
+                <p>Gender: {this.state.people.gender}</p>
+                </div>
+            )}  
+            {!this.state.people && this.state.isFound && 'Not Found'}       
         </div>
-        )
-    }
-};
+        )  
+    }        
+}    
+
 
 export default People;
